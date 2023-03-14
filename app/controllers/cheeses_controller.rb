80e43ca122d466c16dc5a0b90b72db1b62586eca
Glen <glen.kimani@student.moringaschool.com>
2023-03-14 13:50:54 +0300

    Completed

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -1,18 +1,18 @@
 class CheesesController < ApplicationController
 
-  # GET /cheeses
+     # GET /cheeses
   def index
     cheeses = Cheese.all
-    render json: cheeses, expect: [:created_at, :updated_at]
+    render json: cheeses, only: [:id, :name, :price, :is_best_seller]
   end
 
   # GET /cheeses/:id
   def show
-  cheese = Cheese.find_by(id: params[:id])
-  if cheese
-    render json: cheese, except: [:created_at, :updated_at], methods: [:summary]
-  else
-    render json: { error: 'Cheese not found' }
+    cheese = Cheese.find_by(id: params[:id])
+    render json: cheese
   end
+
 end
+
+
 
